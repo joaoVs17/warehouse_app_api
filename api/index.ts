f765939a2e26bde3bdac42528138961c86d29df8
joaoVs17 <joaosimao051@gmail.com>
2024-04-18 13:31:00 -0400

    testing json responses

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
+
+app.get("/", (req, res) => res.json({msg: "Hello, Mommy"}));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+
 
 module.exports = app;
